fba54196f8364fe6f0fa598353a060310e73da98
sarankumarunifo <89625600+sarankumarunifo@users.noreply.github.com>
2024-02-08 10:56:53 +0530

    store added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
-import './App.css';
+import CakeContainer from "./Components/CakeContainer";
+import { Provider } from "react-redux";
+import store from "./redux/Cakes/CakeStore";
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
-   
+
+     <CakeContainer/>
     </div>
+    </Provider>
   );
 }
 
---
+++ src/Components/CakeContainer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import {buyCake} from '../redux/index'
+const CakeContainer = (props) => {
+  return (
+    <div>
+        <h2>No. of Cakes : {props.numOfCakes} </h2>
+        <button onClick={props.buycake}>Buy Cake</button>
+    </div>
+  )
+}
+const mapStateToProps=state=>{
+    return{
+        numOfCakes:state.numOfCakes
+    }
+}
+
+const mapDispatchToProps=dispatch=>{
+    return{
+        buyCake:()=>{dispatch(buyCake())}
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(CakeContainer) 
