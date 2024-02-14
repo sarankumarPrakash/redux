1bf535671598128e5c8519e9abbe896aded92fcc
sarankumarunifo <89625600+sarankumarunifo@users.noreply.github.com>
2024-02-14 16:37:39 +0530

    logger

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,11 +1,14 @@
-import { createStore } from "redux";
+import { createStore,applyMiddleware } from "redux";
 import { CakeReducer } from "./CakeReducer";
 import { combineReducers } from "redux";
+import logger from "redux-logger";
 import Reducers from '../IceCreamRedux/Reducers'
+
 
 const rootReducer=combineReducers({
     cakes:CakeReducer,
     iceCream:Reducers
 });
- const store=createStore(rootReducer)
+
+ const store=createStore(rootReducer, applyMiddleware(logger));
  export default store
